d46a5c3cf4c64790581df940dcff58e4707a6c3a
gagandeep <sherry2312.gk@gmail.com>
2020-06-10 18:18:31 -0400

    [gk]preferences added

--- FindWay_Gagandeep_C0768688/ViewController.swift
+++ FindWay_Gagandeep_C0768688/ViewController.swift
@@ -62,5 +62,76 @@ class ViewController: UIViewController ,CLLocationManagerDelegate,MKMapViewDeleg
                
 
 }
+    
+    
+    @IBAction func showRoute(_ sender: UIButton) {
+        if pressed{
+        
+                let alert = UIAlertController(title: "Preferences", message: "Select transit type", preferredStyle:.actionSheet )
+                let auto = UIAlertAction(title: "Automobile", style: .default) { (action) in
+                    self.showDirection(transport: .automobile)
+                }
+                let walk = UIAlertAction(title: "Walking", style: .default) { (action) in
+                    self.showDirection(transport: .walking)
+                }
+                alert.addAction(auto)
+                alert.addAction(walk)
+                self.present(alert, animated: true, completion: nil)
+            
+        }else{
+            let alert = UIAlertController(title: "location  is missing", message: " please select a location", preferredStyle:.actionSheet )
+            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+     func showDirection(transport: MKDirectionsTransportType) {
+             
+                   print("inside button")
+                   
+                   let source = mapView.annotations[0].coordinate
+
+                           
+                           let sourceplacemark = MKPlacemark(coordinate: source)
+                           let destinationplacemark = MKPlacemark(coordinate: destination!)
+                           
+                           let sourceMapItem = MKMapItem(placemark: sourceplacemark)
+                           let destinationMapItem = MKMapItem(placemark: destinationplacemark)
+
+                           
+                           let directionRequest = MKDirections.Request()
+                           directionRequest.source = sourceMapItem
+                           directionRequest.destination = destinationMapItem
+                           directionRequest.transportType = transport
+                   
+                   print("after direction")
+                           
+                           let directions = MKDirections(request: directionRequest)
+                           
+                           directions.calculate { (response, error) in
+                               guard let response = response else{
+                                   if let error = error{
+                                    let alert = UIAlertController(title: "OOPS", message: "Directions not available", preferredStyle: .alert)
+                                    let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                                    alert.addAction(okAction)
+                                    self.present(alert, animated: true, completion: nil)
+                                       print("Error: \(error)")
+                                   }
+                                   return
+                               }
+                               
+                               print("in calculate")
+                               
+                               let route = response.routes[0]
+                            self.mapView.addOverlay((route.polyline))
+    //                           self.mapView.addOverlay((route.polyline), level: MKOverlayLevel.aboveRoads)
+                               print("add overlay")
+                               
+                               let rect = route.polyline.boundingMapRect
+                               self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+                               print("set region")
+                           
+                           }
+        }
 
 }
