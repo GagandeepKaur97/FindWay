e1492007ad559ef34190975d02cee32dc84e6c22
gagandeep <sherry2312.gk@gmail.com>
2020-06-10 18:05:40 -0400

    [gk]annotations added

--- FindWay_Gagandeep_C0768688/ViewController.swift
+++ FindWay_Gagandeep_C0768688/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController ,CLLocationManagerDelegate,MKMapViewDeleg
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
-    
+    var destination :CLLocationCoordinate2D?
+    var pressed = false
+    var count = 0
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +26,41 @@ class ViewController: UIViewController ,CLLocationManagerDelegate,MKMapViewDeleg
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         
-       
+
+       let tapgesture = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+       tapgesture.numberOfTapsRequired = 2
+       mapView.addGestureRecognizer(tapgesture)
             
             
         }
-    
+    @objc func doubleTapped(gesture: UIGestureRecognizer) {
+          
+          let alloverlays = mapView.overlays
+          
+          if alloverlays.count>0{
+              mapView.removeOverlays(alloverlays)
+          }
+          
+          count += 1
+          pressed = true
+          
+          let allannotations = mapView.annotations
 
+                        if allannotations.count > 1{
+                            let ann = allannotations[1]
+                            mapView.removeAnnotation(ann)
+                        }
+
+        let touchPoint = gesture.location(in: mapView)
+               let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+
+               let annotation = MKPointAnnotation()
+               annotation.coordinate = coordinate
+               mapView.addAnnotation(annotation)
+               
+               destination = coordinate
+               
 
 }
 
+}
