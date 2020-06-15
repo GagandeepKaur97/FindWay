cf0f04502b3ada6fd4fc59305023f673f9c5f3d5
gagandeep <sherry2312.gk@gmail.com>
2020-06-15 10:47:40 -0400

    [gk] added zooming and zoom out button

--- FindWay_Gagandeep_C0768688/AddPlaceVC.swift
+++ FindWay_Gagandeep_C0768688/AddPlaceVC.swift
@@ -185,7 +185,21 @@ class AddPlaceVC: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+    }}*/
+     @IBAction func zoomIn(_ sender: UIButton) {
+        var zoomRegion = mapView.region
+                     zoomRegion.span.latitudeDelta = zoomRegion.span.latitudeDelta/2
+                     zoomRegion.span.longitudeDelta = zoomRegion.span.longitudeDelta/2
+                     mapView.setRegion(zoomRegion, animated: true)
+     
     }
-    */
+
+    @IBAction func zoomOut(_ sender: UIButton) {
+        var zoomRegion = mapView.region
+        zoomRegion.span.latitudeDelta = zoomRegion.span.latitudeDelta*2
+        zoomRegion.span.longitudeDelta = zoomRegion.span.longitudeDelta*2
+        mapView.setRegion(zoomRegion, animated: true)
+        
+    }
 
 }
